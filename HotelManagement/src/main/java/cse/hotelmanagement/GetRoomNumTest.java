2105630e407479f91fdd5185a9332ff3687cd3d2
suhoon1020 <suhoon1020@naver.com>
2023-11-24 14:31:25 +0900

    승원 수훈 합본

---
+++ HotelManagement/src/main/java/cse/hotelmanagement/GetRoomNumTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cse.hotelmanagement;
+
+/**
+ *
+ * @author 심수훈
+ */
+public class GetRoomNumTest {
+
+    /**
+     */
+    public void testCode(String roomNum) {
+        // TODO code application logic here
+        ReserveRoom rr =new ReserveRoom();
+        
+        System.out.println(roomNum);
+        
+    }
+    
+}
